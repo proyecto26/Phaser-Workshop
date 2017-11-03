4163630666057f651345b0e027f649da05180e41
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2017-11-03 07:58:18 -0500

    Add client server basic interaction

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  gameWidth: 1200,
+  gameHeight: 800,
+  localStorageName: 'phaser-template',
+  serverUrl: __DEV__ ? 'http://localhost:8081' : 'https://phaser-realtime-basic-server.herokuapp.com'
+}
---
+++ server/index.js
@@ -0,0 +1,26 @@
+/**
+ * Phaser Real-Time Template
+ * @author       Juan Nicholls <jdnichollsc@hotmail.com>
+ * @copyright    2017 Proyecto 26 - https://github.com/proyecto26/Phaser-Workshop
+ * @license      {@link http://opensource.org/licenses/MIT}
+ * @version 1.0.0
+ */
+
+var express = require('express')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var Player = require('./socket/player')
+
+io.on('connection', (socket) => {
+
+  const player = new Player(socket)
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('enemy.disconnect', socket.id)
+    socket.emit('player.disconnect')
+  })
+})
+
+server.listen(process.env.PORT || 8081, () => {
+  console.log('Listening on '+server.address().port)
+})
